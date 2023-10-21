18aab8c4c2fbd7854c6d9ae286efee78427188d3
Diego Meyer <diego.meyer@xorlab.com>
2023-10-21 22:50:53 +0200

    Add more scripts

---
+++ create-vm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+virt-install \
+  --name myVM \
+  --ram 1024 \
+  --disk path=focal-server-cloudimg-amd64.img,format=qcow2 \
+  --disk path=cloud-init.iso,device=cdrom \
+  --import \
+  --network bridge:virbr0 \
+  --graphics none \
+  --os-variant=ubuntu20.04 \
+  --console pty,target_type=serial
+
