e1112887278e6794f2795966cf89baf758b03329
Diego Meyer <diego.meyer@xorlab.com>
2023-10-21 22:14:55 +0200

    Add huge page info

--- hugepage_info.sh
+++ hugepage_info.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-# Function to display information
-show_info() {
-    local description=$1
-    local file_path=$2
-    local filter_pattern=$3
+# Read and display information from a specific path
+read_info() {
+    local file_path=$1
+    local grep_pattern=$2
 
-    echo "==> $description (from $file_path)"
-    if [[ -z "$filter_pattern" ]]; then
+    echo "Reading from: $file_path"
+    if [ -z "$grep_pattern" ]; then
         cat "$file_path"
     else
-        grep -E "$filter_pattern" "$file_path"
+        grep "$grep_pattern" "$file_path"
     fi
-    echo
+    echo "--------------------------------------------"
 }
 
-# Display total number of huge pages
-show_info "Total Number of Huge Pages" "/proc/sys/vm/nr_hugepages"
+# Total number of huge pages
+read_info "/proc/sys/vm/nr_hugepages"
 
-# Display the default huge page size
-show_info "Huge Page Size" "/proc/meminfo" "Hugepagesize"
+# Huge page size
+read_info "/proc/meminfo" "Hugepagesize"
 
-# Display the total, free, and used huge pages
-show_info "Huge Pages Info" "/proc/meminfo" "HugePages_(Total|Free|Used)"
-
+# Total, free, and used huge pages info
+read_info "/proc/meminfo" "HugePages_"
 
