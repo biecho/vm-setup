64163cfed316ea6dfce2b490e2ff74aaab839907
Biecho <dimeyer@student.ethz.ch>
2023-11-08 15:09:26 +0000

    Modify scrips

--- create-vm.sh
+++ create-vm.sh
@@ -1,13 +1,41 @@
 #!/bin/bash
 
+# Ensure the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run this script as root or with sudo."
+    exit 1
+fi
+
+# Check if virt-install is installed
+if ! command -v virt-install &> /dev/null; then
+    echo "virt-install not found. Installing it now..."
+    apt update
+    apt install -y virtinst
+fi
+
+# Set permissions to allow libvirt-qemu access
+VM_PATH="/home/biecho/vm-setup"
+chmod o+x /home/biecho
+chown -R biecho:libvirt-qemu $VM_PATH
+chmod 0750 $VM_PATH
+find $VM_PATH -type f -exec chmod 0640 {} \;
+find $VM_PATH -type d -exec chmod 0750 {} \;
+
+# Create the VM using virt-install
 virt-install \
   --name biecho \
-  --ram 1024 \
-  --disk path=focal-server-cloudimg-amd64.img,format=qcow2 \
-  --disk path=cloud-init.iso,device=cdrom \
+  --ram 2048 \
+  --disk path=${VM_PATH}/focal-server-cloudimg-amd64.img,format=qcow2 \
+  --disk path=${VM_PATH}/cloud-init.iso,device=cdrom \
   --import \
   --network bridge:virbr0 \
   --graphics none \
   --os-variant=ubuntu20.04 \
   --console pty,target_type=serial
 
+# Check the exit status of the virt-install command
+if [ $? -eq 0 ]; then
+    echo "Virtual machine 'biecho' has been created."
+else
+    echo "An error occurred while creating the virtual machine. Please check the error messages above."
+fi
--- generate-cloud-init-iso.sh
+++ generate-cloud-init-iso.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Check if genisoimage is installed
+if ! command -v genisoimage &> /dev/null; then
+    echo "genisoimage not found. Installing it now..."
+    sudo apt update
+    sudo apt install -y genisoimage
+fi
+
+# Create the ISO for cloud-init
 genisoimage -output cloud-init.iso -volid CIDATA -joliet -rock user-data meta-data
 
+echo "cloud-init.iso has been created."
